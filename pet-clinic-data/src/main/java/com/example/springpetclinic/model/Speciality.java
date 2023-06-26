18b9377357baa21607792074ff691de518aadac3
ana <ana.aldea.c@gmail.com>
2023-06-26 17:23:27 +0300

    Updated vet to use JPA. Closes 37.

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Speciality.java
@@ -1,7 +1,13 @@
 package com.example.springpetclinic.model;
 
-public class Speciality extends BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "specialties")
+public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
