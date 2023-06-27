e8594cded95e86ca39c6e49275c5e1be4a83f07e
ana <ana.aldea.c@gmail.com>
2023-06-27 14:25:00 +0300

    Updated profiles for JPA service, tested active profiles. Closes #48.

--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/OwnerService.java
@@ -1,7 +1,9 @@
 package com.example.springpetclinic.services;
 
 import com.example.springpetclinic.model.Owner;
+import org.springframework.context.annotation.Profile;
 
+@Profile({"default", "map"})
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 
